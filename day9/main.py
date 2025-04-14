c3a3ee18726714eb4a6f2e1d58cf3e5d516bcd90
gulshan07 <gulshanyadavqaz@gmail.com>
2025-04-14 15:33:48 +0530

    function with parameter

---
+++ day9/main.py
@@ -0,0 +1,8 @@
+def printt(name):
+    print(f"hello {name} ")
+    print(f"How do you do {name}")
+    print(f"Isn't the weather nice today, {name}?")
+name= input("Please enter your name: ")
+printt(name)
+
+
