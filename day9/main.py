e70d9d24a891aed468b9463c9ca31ba07823bec2
gulshan07 <gulshanyadavqaz@gmail.com>
2025-04-14 15:39:39 +0530

    function with parameters

--- day9/main.py
+++ day9/main.py
@@ -1,8 +1,16 @@
-def printt(name):
-    print(f"hello {name} ")
-    print(f"How do you do {name}")
-    print(f"Isn't the weather nice today, {name}?")
-name= input("Please enter your name: ")
-printt(name)
+# def printt(name):
+#     print(f"hello {name} ")
+#     print(f"How do you do {name}")
+#     print(f"Isn't the weather nice today, {name}?")
+# name= input("Please enter your name: ")
+# printt(name)
 
 
+#FUNCTION with more inputs
+def greet(name, game):
+    print(f"hello {name} is your favourite game is {game} ")
+
+
+name=input("name: ")
+game=input("hobby: ")
+greet(name,game)
