2095b7e7e96b733d541c6fda144cf046f1a31b61
gulshan07 <gulshanyadavqaz@gmail.com>
2025-04-18 14:34:33 +0530

    main py

--- day11/main.py
+++ day11/main.py
@@ -1,6 +1,12 @@
 #funcition with outputs
-def my_function():
-    result=3*2
-    return result
-ans=my_function()
-print(ans)
+# def my_function():
+#     result=3*2
+#     return result
+# ans=my_function()
+# print(ans)
+
+
+def format_name(f_name,l_name):
+    print(f_name.title())
+    print(l_name.title())
+format_name("angellaa","AGNGELGA")
